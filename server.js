73258b17065180245f0f87fc24d501e00ef9414e
GBSE83 <gbse838383@gmail.com>
2025-03-26 20:47:28 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
+const { ExpressPeerServer } = require('peer');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,10 +9,25 @@ const PORT = process.env.PORT || 3000;
 app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'landing.html'));
+// Configura PeerJS Server
+const server = app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor en http://localhost:${PORT}`);
+const peerServer = ExpressPeerServer(server, {
+  path: '/peerjs',
+  proxied: true
+});
+
+const peerConfig = {
+    host: 'https://dicesandchallenges.onrender.com',
+    path: '/peerjs',
+    secure: true,
+    port: 443
+};
+
+app.use('/peerjs', peerServer);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
